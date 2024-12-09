6eef89b71eb1706f37f04d2046d89d46665ae91f
Palaolitico <p@seismotechnologies.com>
2024-12-09 14:06:36 +0100

    refactor: make CursorOf a template

--- src/main/java/org/seismotech/ground/cursor/IntCursor.java
+++
@@ -1,7 +0,0 @@
-package org.seismotech.ground.cursor;
-
-public interface IntCursor extends CursorOf<Integer> {
-  int intValue();
-
-  @Override default Integer value() {return intValue();}
-}
--- src/main/java/org/seismotech/ground/cursor/LongCursor.java
+++
@@ -1,7 +0,0 @@
-package org.seismotech.ground.cursor;
-
-public interface LongCursor extends CursorOf<Long> {
-  long longValue();
-
-  @Override default Long value() {return longValue();}
-}
